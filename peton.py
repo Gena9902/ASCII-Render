9861e1e31ada83c40f4b41924cfd0606be8b968d
Gena Ivaschenko <gena9902@gmail.com>
2022-01-29 17:19:23 +0600

    add function for umnojenie

--- peton.py
+++ peton.py
@@ -7,5 +7,18 @@ def summ(a):
         summa += a[i]
     return summa
 
-in_a = [int(i) for i in input().split()]
-print(summ(in_a))
+def umnojit(a):
+    out_um = 1
+    for i in range(len(a)):
+        out_um *= a[i]
+    return out_um
+
+in_a = [int(i) for i in input("Введи числа: ").split()]
+choice = int(input("1 для умножить 2 для сложить: "))
+
+if choice == 1:
+    print(umnojit(in_a))
+elif choice == 2:
+    print(summ(in_a))
+else:
+    print("ERROR")
