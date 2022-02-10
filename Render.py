5e07f2e637072381cb9bd2f08224b459362b46f3
Gena Ivaschenko <gena9902@gmail.com>
2022-02-10 14:58:50 +0600

    Add ANSI code and font color

--- Render.py
+++ Render.py
@@ -1,5 +1,8 @@
 import os
 import termcolor as tc
+import ctypes
+kernel32 = ctypes.windll.kernel32
+kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
 
 class ASCIIRender(object):
 
@@ -12,14 +15,19 @@ class ASCIIRender(object):
         
         return size
 
-    def font(self, color, size_in):
+    def font(self, color, on_color, size_in):
         size = size_in
-        font = [' ' for i in range(size[0] * (size[1]-1))]
+        font = [' ' for i in range(size[0] * (size[-1]))]
         for i in font:
-            tc.cprint(i, 'color', end='', sep='')
+            tc.cprint(i, color, on_color, end='', sep='')
 
     def Render(ColorBuffer, sizeinfunction):
         ColorBuffer
         size = sizeinfunction
         #for i in range(size[0] * (size[1]-1)):
         #    ColorBuffer[]
+
+
+ASCII_Instance = ASCIIRender()
+size = ASCII_Instance.size()
+ASCII_Instance.font('white', 'on_white', size)
